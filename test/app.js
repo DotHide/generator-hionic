2e8f70def6ccff6dbeac91a40a2f63b0276591cf
Jack Zhang <dothide@gmail.com>
2016-06-11 22:16:16 +0800

    Update test

--- test/app.js
+++ test/app.js
@@ -6,7 +6,6 @@ var helpers = require('yeoman-test');
 describe('generator-hionic:app', function() {
   before(function(done) {
     helpers.run(path.join(__dirname, '../app'))
-      .withPrompts({ someAnswer: true })
       .on('end', done);
   });
 
