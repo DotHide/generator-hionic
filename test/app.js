455a7a8f33cf4266125f79f0bb19ff4d270c6089
Jack Zhang <dothide@gmail.com>
2016-06-12 00:03:21 +0800

    Update test file according to angular

--- test/app.js
+++ test/app.js
@@ -4,25 +4,52 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-hionic:app', function() {
-  before(function(done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .on('end', done);
+  beforeEach(function() {
+    this.hionic = helpers
+      .run(require.resolve('../app'));
   });
 
-  it('Package files', function() {
-    assert.file([
-      'bower.json',
-      'config.xml',
-      'Gruntfile.js',
-      'package.json'
-    ]);
+  describe('create project structure', function() {
+    beforeEach(function(done) {
+      this.hionic.on('end', done);
+    });
+
+    it('Package files', function() {
+      assert.file([
+        'bower.json',
+        'config.xml',
+        'Gruntfile.js',
+        'package.json'
+      ]);
+    });
+
+    it('App folder', function() {
+      assert.file([
+        'app/index.html',
+        'app/scripts/app.module.js',
+        'app/scss/styles.scss'
+      ]);
+    });
   });
 
-  it('App folder', function() {
-    assert.file([
-      'app/index.html',
-      'app/scripts/app.module.js',
-      'app/scss/styles.scss'
-    ]);
+  describe('--appName', function() {
+    beforeEach(function(done) {
+      this.hionic
+        .withArguments(['appName'])
+        .on('end', done);
+    });
+
+    it('generates appName in project files', function() {
+      assert.fileContent(
+        'app/scripts/app.module.js',
+        /module\('appName'/
+      );
+
+      assert.fileContent(
+        'app/index.html',
+        /ng-app="appName"/
+      );
+    });
   });
+
 });
